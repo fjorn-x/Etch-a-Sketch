d969ac7f65411653b279ec54a8e7350352f4fc4a
fjorn-x <hamzashaikh00@gmail.com>
2022-06-26 17:34:42 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const DEFAULT_COLOR = "green";
+
+const board = document.querySelector(".board");
+const setSize = document.querySelector(".set-size");
+const sizeSlider = document.querySelector(".size-slider");
+const sizeIndicator = document.querySelector(".size-indicator");
+const colorPicker = document.querySelector(".color-pick");
+const clearScreen = document.querySelector(".clear");
+const eraser = document.querySelector(".eraser");
+
+let color = DEFAULT_COLOR;
+let gridSize = sizeSlider.value;
+let isDragging = false;
+
+sketchPad(gridSize);
+
+sizeIndicator.textContent = `${gridSize} x ${gridSize}`;
+
+function sketchPad(gridSize) {
+  board.style.gridTemplateRows = `repeat(${gridSize},1fr)`;
+  board.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
+  for (let i = 0; i < gridSize ** 2; i++) {
+    const grid = document.createElement("div");
+    grid.classList.add("grid-item");
+    board.appendChild(grid);
+    grid.addEventListener("mousedown", (e) => {
+      isDragging = true;
+    });
+    grid.addEventListener("mouseover", (e) => {
+      if (isDragging === true) {
+        colorChanger(grid);
+      }
+    });
+    grid.addEventListener("mouseup", (e) => {
+      if (isDragging === true) {
+        isDragging = false;
+        colorChanger(grid);
+      }
+    });
+  }
+}
+
+function newSketch() {
+  board.textContent = "";
+  gridSize = sizeSlider.value;
+  sketchPad(gridSize);
+  sizeIndicator.innerHTML = `${gridSize} x ${gridSize}`;
+}
+
+function colorChanger(grid) {
+  grid.style.setProperty("--color", `${color}`);
+}
+
+sizeSlider.addEventListener("input", () => {
+  newSketch();
+});
+colorPicker.addEventListener("input", () => {
+  color = colorPicker.value;
+});
+eraser.addEventListener("click", () => {
+  color = "white";
+});
+
+clearScreen.addEventListener("click", () => {
+  color = DEFAULT_COLOR;
+  newSketch();
+});
